39b40ccacca2491495bca8ca76490b047784cfa3
Rudra Banerjee <71057615+RudraIITG@users.noreply.github.com>
2024-08-28 00:06:02 +0530

    Update Scrape_data.py

--- Scrape_data.py
+++ Scrape_data.py
@@ -4,38 +4,38 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import Config_topic
-# st.title("Webscraping App")
 
 def scrape(topic):
-  newz_collab=[]
-  base_url = 'https://news.google.com/'
-  response_=requests.get(f"https://news.google.com/search?q={topic}&hl=en-IN&gl=IN&ceid=IN%3Aen")
-  response=response_.content
-  soup=BeautifulSoup(response,"lxml")
-  news=soup.find_all("article",class_="IFHyqb DeXSAc")
-  for newz in news:
-    newspaperName=newz.find("div",class_="vr1PYe").text
-    newsheadLine=newz.find("a", class_="JtKRv").text
-    time=newz.find("time", class_="hvbAAd").text
-    link_to_article=newz.a['href']
-    real_link= base_url+link_to_article
+    newz_collab = []
+    base_url = 'https://news.google.com/'
+    response_ = requests.get(f"https://news.google.com/search?q={topic}&hl=en-IN&gl=IN&ceid=IN%3Aen")
+    response = response_.content
+    soup = BeautifulSoup(response, "lxml")
+    news = soup.find_all("article", class_="IFHyqb DeXSAc")
+    
+    for newz in news:
+        newspaperName = newz.find("div", class_="vr1PYe").text
+        newsheadLine = newz.find("a", class_="JtKRv").text
+        time = newz.find("time", class_="hvbAAd").text
+        link_to_article = newz.a['href']
+        real_link = base_url + link_to_article
 
-    newz_collab.append([newspaperName,newsheadLine,time,real_link])
-  return newz_collab
+        # Create a clickable link
+        clickable_link = f'<a href="{real_link}" target="_blank">click here</a>'
 
-
+        newz_collab.append([newspaperName, newsheadLine, time, clickable_link])
+    
+    return newz_collab
 
 def stream_data(welcome):
     for word in welcome.split(" "):
-        yield word + "  "  # yeild keyword is used to create a generator function, A type of function that is memory efficient and can be used as iterator object
+        yield word + "  "
         time.sleep(0.1)
-
 
 welcome = "Thank You for using our webapp"
 
-
 def app():
-    st.title("Scrapping the data From Multiple Websites")
+    st.title("Scraping Data From Multiple Websites")
     Config_topic.topic = st.text_input("Enter the topic to scrape for news:")
     st.write_stream(stream_data(welcome))
 
@@ -43,21 +43,28 @@ def app():
         if Config_topic.topic:
             news_data = scrape(Config_topic.topic)
             if news_data:
-
-                df = pd.DataFrame(news_data,columns=["newspaperName", "newsheadLine", "time", "link_To_Article"])
+                df = pd.DataFrame(news_data, columns=["Newspaper Name", "News Headline", "Time", "Link To Article"])
+                
+                # Save to CSV
                 csv_file = '../news_data.csv'
                 df.to_csv(csv_file, index=True)
-
                 st.success('Scraping successful! Data saved to news_data.csv.')
-                st.dataframe(df)
+
+                # Display the DataFrame with clickable links
+                st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
+                
+                # Provide download button
                 Config_topic.news_Number += 1
-                st.download_button(label="Download CSV", data=df.to_csv(index=False), file_name='../news_data_{}.csv'.format(Config_topic.news_Number),
-                                   mime='text/csv')
+                st.download_button(
+                    label="Download CSV", 
+                    data=df.to_csv(index=False), 
+                    file_name='../news_data_{}.csv'.format(Config_topic.news_Number),
+                    mime='text/csv'
+                )
             else:
                 st.warning('No news found for the given topic.')
         else:
             st.error('Please enter a topic.')
-
 
 if __name__ == "__app__":
     app()
