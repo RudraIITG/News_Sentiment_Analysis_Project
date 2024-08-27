3b34f95bda8c300c7287d82d6e88d609950f236c
Rudra Banerjee <71057615+RudraIITG@users.noreply.github.com>
2024-08-28 00:08:08 +0530

    Update Scrape_data.py

--- Scrape_data.py
+++ Scrape_data.py
@@ -12,7 +12,6 @@ def scrape(topic):
     response = response_.content
     soup = BeautifulSoup(response, "lxml")
     news = soup.find_all("article", class_="IFHyqb DeXSAc")
-    
     for newz in news:
         newspaperName = newz.find("div", class_="vr1PYe").text
         newsheadLine = newz.find("a", class_="JtKRv").text
@@ -20,19 +19,21 @@ def scrape(topic):
         link_to_article = newz.a['href']
         real_link = base_url + link_to_article
 
-        # Create a clickable link
+        # Make the link clickable in HTML format
         clickable_link = f'<a href="{real_link}" target="_blank">click here</a>'
 
         newz_collab.append([newspaperName, newsheadLine, time, clickable_link])
-    
     return newz_collab
+
 
 def stream_data(welcome):
     for word in welcome.split(" "):
-        yield word + "  "
+        yield word + "  "  # yield creates a generator function
         time.sleep(0.1)
 
+
 welcome = "Thank You for using our webapp"
+
 
 def app():
     st.title("Scraping Data From Multiple Websites")
@@ -43,28 +44,23 @@ def app():
         if Config_topic.topic:
             news_data = scrape(Config_topic.topic)
             if news_data:
-                df = pd.DataFrame(news_data, columns=["Newspaper Name", "News Headline", "Time", "Link To Article"])
-                
-                # Save to CSV
+                df = pd.DataFrame(news_data, columns=["newspaperName", "newsheadLine", "time", "link_To_Article"])
                 csv_file = '../news_data.csv'
                 df.to_csv(csv_file, index=True)
+
                 st.success('Scraping successful! Data saved to news_data.csv.')
 
                 # Display the DataFrame with clickable links
                 st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
-                
-                # Provide download button
+
                 Config_topic.news_Number += 1
-                st.download_button(
-                    label="Download CSV", 
-                    data=df.to_csv(index=False), 
-                    file_name='../news_data_{}.csv'.format(Config_topic.news_Number),
-                    mime='text/csv'
-                )
+                st.download_button(label="Download CSV", data=df.to_csv(index=False),
+                                   file_name='../news_data_{}.csv'.format(Config_topic.news_Number), mime='text/csv')
             else:
                 st.warning('No news found for the given topic.')
         else:
             st.error('Please enter a topic.')
+
 
 if __name__ == "__app__":
     app()
